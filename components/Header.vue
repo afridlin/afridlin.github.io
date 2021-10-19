4647f968ebb7faeae81baf387eac478c81524ae5
Alexander <alex.fridlin@gmail.com>
2021-10-19 18:24:14 +0200

    dont know

---
+++ components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <nav>
+      <div class="container">
+        <div
+          class="is-flex is-justify-content-space-around is-align-items-center py-4"
+        >
+          <figure class="image is-128x128">
+            <img src="/profile.png" class="is-rounded" />
+          </figure>
+          <a class="button is-large is-primary" href="#"> Start </a>
+          <a class="button is-large is-primary" href="#"> Projekte </a>
+          <a class="button is-large is-primary" href="#"> Lebenslauf </a>
+          <a class="button is-large is-primary" href="#"> Skills </a>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <section>
+    <div class="container">
+      <h1>hello world</h1>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
